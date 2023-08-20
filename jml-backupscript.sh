576f2f84909d8666bf3dae5c0fd7b2c2f0461d96
Joakim <jockemedlinux@gmail.com>
2023-08-20 16:27:09 +0200

    Updates to the backup-script

--- jml-backupscript.sh
+++ jml-backupscript.sh
@@ -7,9 +7,15 @@
 
 ### DEVICES AND FILESYSTEMS
 backupdevice=/dev/sda
+
+#backupdirectory=/mnt/SSD/
+#dockerbackupdir=/mnt/SSD/
+
 backupdirectory=/mnt/DATADISK/_BACKUPS/server/thinker-server
-name=thinker_
-fileoutput=$name$(hostname)_$(date +%F).img
+dockerbackupdir=/mnt/DATADISK/_BACKUPS/dockers/
+dockerimagenames=$(docker images --format {{.Repository}})
+dockercontainernames=$(docker ps --format {{.Names}})
+fileoutput=$(hostname)_$(date +%F).img
 
 ### LOOP DEVICES
 loop=/dev/loop0
@@ -35,7 +41,18 @@ checkpoint() {
     fi
     echo -e "\e[92m[+] [PASSED]: $1\e[0m\n[-------------------------------------------------------------------------------------------]"
     ## UNCOMMENT TO SLOW DOWN SCRIPT.
-    # sleep 5
+    sleep 5
+}
+
+checkFilesystem() {
+	e2fsck -pf $loopdev
+	(( $? < 4 )) && return
+	
+	echo -e "\n\e[91m[ERROR] Found filesystem error.\e[0m"
+	echo -e "\e[91m[ERROR] Trying to fix\e[0m\n"
+	e2fsck -y $loopdev
+	e2fsck -pf
+	(( $? < 4 )) && return
 }
 
 ####################################################################################
@@ -51,7 +68,7 @@ fi
 checkpoint "Sudo permissions used"
 
 ### NEEDED PACKAGES
-packages="pv e2fsck dumpe2fs resize2fs truncate docker losetup smbd"
+packages="pv e2fsck dumpe2fs resize2fs truncate losetup smbd docker"
 echo -e "\e[93m[INFO] Checking needed packages and programs\e[0m\n"
 for line in $packages; do
     if which $line >/dev/null; then
@@ -64,9 +81,33 @@ done
 echo -e ""
 checkpoint "Found all needed settings and programs"
 
+echo -e "\e[93m[INFO] Do you also want to backup your dockers and volumes? [Y/N]: \e[0m"
+read danswer
+if [[ $danswer == "Y" || $danswer == "y" ]]; then
+   	echo -e "\e[93m[INFO] Committing latest docker containers\e[0m"
+   	mkdir -p $dockerbackupdir/$(date +%F)
+   	docker start $(docker ps -a -q)
+   	for container in $dockercontainernames; do
+   		docker commit -p $container "jml-$container:latest"
+    done
+    echo -e "\e[93m[INFO] Backing up latest docker images \e[0m"
+	for dockerimage in $dockerimagenames; do
+		docker save $dockerimage | pv -tpre > "$dockerbackupdir/$(date +%F)/$dockerimage.tar"
+	done
+
+	echo -e "\e[93m[INFO] Backing up docker directories. (Will prune dangling dockers) \e[0m"
+	tar czf "$dockerbackupdir/$(date +%F)/docker-directories.tar.gz" /srv/
+	echo "Y" | docker system prune
+	checkpoint "Backing up docker containers, volumes, and images"
+
+else
+   echo -e "\e[93m[INFO] Skipping docker backups\e[0m"
+   checkpoint "Skipping docker backup"
+fi
+
 ### STOP SERVICES AND DOCKERS
 echo -e "\e[93m[INFO] Stopping services and dockers\e[0m"
-docker stop $(docker ps -a -q)
+docker stop -s 9 $(docker ps -a -q)
 checkpoint "Stopping services and dockers"
 
 ### CHECK BACKUP-FILE BEFORE BACKUP DEVICE
@@ -75,21 +116,24 @@ if [[ -e "$backupdirectory/$fileoutput" ]]; then
     read answer
     if [[ $answer == "Y" || $answer == "y" ]]; then
         echo -e "\e[93m[INFO] Overwriting existing backup file...\e[0m"
-        pv -tpreb $backupdevice | dd of=$backupdirectory/$fileoutput bs=1M conv=noerror,sparse
+        pv -tpreb $backupdevice | dd of=$backupdirectory/$fileoutput bs=200M conv=noerror iflag=fullblock
+        chmod 777 $backupdirectory/$fileoutput
         checkpoint "Backing up device"
     else
         echo -e "\e[93m[INFO] Skipping backup.\e[0m"
         checkpoint "Skipping backup"
     fi
 else
-	echo -e "\e[93m[INFO] Overwriting existing backup file...\e[0m"
-	pv -tpreb $backupdevice | dd of=$backupdirectory/$fileoutput bs=1M conv=noerror,sparse
+	echo -e "\e[93m[INFO] Backing up filessytem on device: $backupdevice \e[0m"
+	pv -tpreb $backupdevice | dd of=$backupdirectory/$fileoutput bs=200M conv=noerror iflag=fullblock
+	chmod 777 $backupdirectory/$fileoutput
 	checkpoint "Backing up device"
 fi
 
 ### FIND ORIGINAL FILESIZE
 echo -e "\e[93m[INFO] Checking filesize\e[0m"
 findsize=$(ls $backupdirectory/$fileoutput -lhS | awk '{print $5}')
+
 if [[ -z $findsize ]]; then
 	abort "File not found $1"
 fi
@@ -97,19 +141,19 @@ echo -e "\tFilesize: $findsize"
 checkpoint "Find original filesize"
 
 ### LOOP AND MINIMIZE DEVICE FILESYSTEM
-sleep 5
-echo -e "\e[93m[INFO] Check filesystem #1\e[0m"
+echo -e "\e[93m[INFO] Checking filesystem #1\e[0m"
 losetup -f -P $backupdirectory/$fileoutput
-sleep 5
-e2fsck -pv $loopdev
-checkpoint "Check filesystem #1"
+checkpoint "Mounted loopdevices"
+
+checkFilesystem
+checkpoint "Checking filesystem #1"
 
 ### GATHER necessary INFO
 echo -e "\e[93m[INFO] Gathering necessary information\e[0m"
 blocksize=`dumpe2fs -h $loopdev | grep "Block size:" | awk '{print $3}'`
 minsize=`resize2fs -P $loopdev | grep -o "[0-9]\+"`
 start=`fdisk -lu | grep $loopdev | awk '{print $2}'`
-startsize=`expr $start + 100`
+startsize=`expr $start + 1`
 minsizeconv=`expr $minsize \* 4096 / 512`
 partend=`expr $startsize + $minsizeconv`
 truncsize=`expr $partend \* 512`
@@ -117,7 +161,7 @@ truncsize=`expr $partend \* 512`
 echo -e "\n\e[93m[INFO] r2fs blocksize:\t\t$blocksize\t\t[4K blocks]\e[0m"
 echo -e "\e[93m[INFO] r2fs minsize:\t\t$minsize\t\t[4K blocks]\e[0m"
 echo -e "\e[93m[INFO] Start Sectors:\t\t$start\t\t[512 blocks]\e[0m"
-echo -e "\e[93m[INFO] Start Sectors(+1):\t$startsize\t\t[51./2 blocks]\e[0m"
+echo -e "\e[93m[INFO] Start Sectors(+1):\t$startsize\t\t[512 blocks]\e[0m"
 echo -e "\e[93m[INFO] Minsize Converted:\t$minsizeconv\t[512 blocks]\e[0m"
 echo -e "\e[93m[INFO] Partition new end:\t$partend\t[512 blocks]\e[0m"
 echo -e "\e[93m[INFO] Truncsize:\t\t$truncsize\t[bytes]\e[0m\n"
@@ -125,6 +169,7 @@ checkpoint "Gathering necessary informations"
 
 ############################################################################################
 ### DEBUG SECTION. UNCOMMENT TO STOP SCRIPT HERE BEFORE IRREVOCABLY TRUNCATE/RESIZE BACKUP FILE
+
 #echo -e "\e[93m\n\nExiting script for improvements\n\n\e[0m"
 #losetup -D
 #exit 0
@@ -137,8 +182,16 @@ checkpoint "Resizing filesystem"
 
 ### CREATE NEW PARTITION SIZE ACCORDINGLY
 echo -e "\e[93m[INFO] Creating the new partition\e[0m"
-echo -e 'd\n2\nn\np\n2\n\n'$partend'\nw' | fdisk $loop 
-checkpoint "Create the new partition"
+echo -e 'd\n\nn\n\n\n'$partend'\n\nw' | fdisk $loop
+checkpoint "Created the new partition. Unallocated space is now ready to be truncated"
+
+echo -e "\e[93m[INFO] Check the filesystem #2\e[0m"
+checkFilesystem
+checkpoint "Checking filesystem #2"
+
+echo -e "\e[93m[INFO] Unmounting loopdevices before cutting\e[0m"
+losetup -D
+checkpoint "Loopdevices unmounted"
 
 ### TRUNCATE THE IMAGE FILE TO SMALLEST POSSIBLE SIZE.
 echo -e "\e[93m[INFO] Truncating image file\e[0m"
@@ -146,18 +199,17 @@ truncate --size=$truncsize $backupdirectory/$fileoutput
 checkpoint "Truncating image file"
 
 ### RECHECK FILESYSTEM AFTER TRUNCATION.
-echo -e "\e[93m[INFO] Check the filesystem #2\e[0m"
-e2fsck -pv $loopdev
-checkpoint "Check filesystem #2"
+#echo -e "\e[93m[INFO] Remounting loopdevices for filesystem check after truncated\e[0m"
+#losetup -f -P $backupdirectory/$fileoutput
+#checkpoint "Opened truncated loopdevice"
 
 ### FIND NEW FILESIZE
-echo -e "\e[93m[INFO] Checking filesize\e[0m"
+echo -e "\e[93m[INFO] Checking new filesize\e[0m"
 findnewsize=$(ls $backupdirectory/$fileoutput -lhS | awk '{print $5}')
 if [[ -z $findnewsize ]]; then
 	abort "File not found: $1"
 fi
 echo -e "\t[INFO] Filesize: $findnewsize"
-checkpoint "Find new filesize"
 
 ### STATUS AND OUTPUTS.
 echo -e "\e[93m[INFO] Old filesize:\t$findsize\e[0m"
@@ -171,4 +223,5 @@ echo -e "\e[93m[INFO] Restarting dockers and services\e[0m"
 docker restart $(docker ps -a -q)
 systemctl restart smbd
 checkpoint "[+] Congratulation! The script finished successfully and services has been restored. BYE!"
+chmod -R 777 "/mnt/DATADISK/_BACKUPS/"
 exit 0
